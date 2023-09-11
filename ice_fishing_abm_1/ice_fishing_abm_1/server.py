95a299c8cce62823ff2ade71740ceb1b7eea23e6
valery <vagechirkov@gmail.com>
2023-09-11 16:23:12 +0200

    create server files

---
+++ ice_fishing_abm_1/ice_fishing_abm_1/server.py
@@ -0,0 +1,48 @@
+import mesa
+import matplotlib as mpl
+
+from .model import Model
+
+cmap = mpl.colormaps['Blues']
+norm = mpl.colors.Normalize(vmin=0, vmax=20)
+m = mpl.cm.ScalarMappable(cmap=cmap, norm=norm)
+
+m_belief = mpl.cm.ScalarMappable(cmap=mpl.colormaps['Blues'], norm=mpl.colors.Normalize(vmin=0, vmax=.05))
+
+
+def draw_grid(agent):
+    """
+    Portrayal Method for canvas
+    """
+    if agent is None:
+        return
+
+    if agent.state == "fishing" and agent.fishing_time == 1:
+        portrayal = {"Shape": "jackhammer.svg", "Layer": 1, "State": agent.state, "Catch": agent.total_catch}
+    elif agent.state == "fishing":
+        portrayal = {"Shape": "fisher-fishing.svg", "Layer": 1, "State": agent.state, "Catch": agent.total_catch}
+    elif agent.state == "moving":
+        portrayal = {"Shape": "fisher-moving.svg", "Layer": 1, "State": agent.state, "Catch": agent.total_catch}
+    else:
+        portrayal = {"Shape": "fisher-fishing.svg", "Layer": 1, "State": agent.state, "Catch": agent.total_catch}
+    return portrayal
+
+
+grid_size = 20
+grid_canvas_size = 600
+
+grid = mesa.visualization.CanvasGrid(
+    draw_grid, grid_size, grid_size, grid_canvas_size, grid_canvas_size)
+
+model_params = {
+    "grid_width": grid_size,
+    "grid_height": grid_size,
+    "number_of_agents": mesa.visualization.Slider("N agents", value=5, min_value=1, max_value=10, step=1),
+
+}
+
+plots = [grid]
+
+server = mesa.visualization.ModularServer(Model, plots,
+                                          "Ice Fishing Model 1", model_params
+                                          )
---
+++ ice_fishing_abm_1/run.py
@@ -0,0 +1,3 @@
+from ice_fishing_abm_1.server import server
+
+server.launch(open_browser=False, port=8522)
