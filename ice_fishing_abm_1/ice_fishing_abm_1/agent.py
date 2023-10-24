f545bdc3799ff99018e9a4ef54c21ae1df747c86
valery <vagechirkov@gmail.com>
2023-10-24 15:02:52 +0200

    refactor agent meso scale

--- ice_fishing_abm_1/ice_fishing_abm_1/agent.py
+++ ice_fishing_abm_1/ice_fishing_abm_1/agent.py
@@ -3,7 +3,7 @@ from typing import Union
 import mesa
 import numpy as np
 
-from .utils import discount_by_distance, find_peak, smooth_with_gaussian_filter
+from .utils import discount_by_distance, find_peak
 
 
 class Agent(mesa.Agent):
@@ -18,7 +18,6 @@ class Agent(mesa.Agent):
             prior_knowledge_noize: float = 0.3,
             w_social: float = 0.4,
             w_personal: float = 0.4,
-            visualization: bool = False,
     ):
         super().__init__(unique_id, model)
         # parameters
@@ -36,39 +35,62 @@ class Agent(mesa.Agent):
         # ---- global displacement weights ----
         self.w_soc, self.w_env, self.w_rand = self._information_weights(w_social, w_personal)
 
-        # ---- debug parameters ----
-        self.visualization: bool = visualization
-
         # states
         # ---- movement-related states ----
-        self.is_moving: bool = False
-        self.destination: Union[None, tuple] = None
+        self._is_moving: bool = False
+        self._destination: Union[None, tuple] = None
 
         # ---- sampling-related states ----
-        self.is_sampling: bool = False
-        self.sampling_sequence: list[int, ...] = []
-        self.observations: np.ndarray = np.ndarray(shape=(model.grid.width, model.grid.height), dtype=float)
-        self.observations.fill(1e-6)
-        self.local_search_count: int = 0
-        self.collected_resource: int = 0
+        self._is_sampling: bool = False
+        self._sampling_sequence: list[int, ...] = []
+        self._observations: np.ndarray = np.ndarray(shape=(model.grid.width, model.grid.height), dtype=float)
+        self._observations.fill(1e-6)
+        self._local_search_count: int = 0
+        self._collected_resource: int = 0
 
-        # ---- meso-scale beliefs ----
-        self.meso_belief: np.ndarray = np.zeros(shape=(self.meso_grid_width, self.meso_grid_height), dtype=float)
-        self.meso_env_belief: np.ndarray = np.zeros(shape=(self.meso_grid_width, self.meso_grid_height), dtype=float)
-        self.meso_soc_density: np.ndarray = np.zeros(shape=(self.meso_grid_width, self.meso_grid_height), dtype=float)
-        self.meso_rand_array: np.ndarray = np.zeros(shape=(self.meso_grid_width, self.meso_grid_height), dtype=float)
+        # ---- meso-scale information ----
+        shape = (self.model.grid.meso_width, self.model.grid.meso_height)
+        self._meso_env: np.ndarray = np.zeros(shape=shape, dtype=float)
+        self._meso_soc: np.ndarray = np.zeros(shape=shape, dtype=float)
+        self._meso_combined: np.ndarray = np.zeros(shape=shape, dtype=float)
 
         self.update_observations_with_prior_knowledge()
 
+    @property
+    def meso_pos(self):
+        return self.model.grid.meso_coordinate(*self.pos)
+
+    @property
+    def meso_soc(self):
+        return np.kron(self._meso_soc, np.ones((self.model.grid.meso_step, self.model.grid.meso_step)))
+
+    @property
+    def meso_env(self):
+        return np.kron(self._meso_env, np.ones((self.model.grid.meso_step, self.model.grid.meso_step)))
+
+    @property
+    def meso_combined(self):
+        return np.kron(self._meso_combined, np.ones((self.model.grid.meso_step, self.model.grid.meso_step)))
+
+    @property
+    def is_moving(self):
+        return self._is_moving
+
+    @property
+    def is_sampling(self):
+        return self._is_sampling
+
     def update_observations_with_prior_knowledge(self):
-        self.meso_env_belief.fill(self.prior_knowledge)
+        # TODO: implement
+        pass
+        # self._meso_env.fill(self.prior_knowledge)
 
     def move(self):
         """
         Move agent one cell closer to the destination
         """
         x, y = self.pos
-        dx, dy = self.destination
+        dx, dy = self._destination
         if x < dx:
             x += 1
         elif x > dx:
@@ -80,10 +102,10 @@ class Agent(mesa.Agent):
         self.model.grid.move_agent(self, (x, y))
 
         # check if destination has been reached
-        if self.pos == self.destination:
-            self.is_moving = False
+        if self.pos == self._destination:
+            self._is_moving = False
             # start sampling
-            self.is_sampling = True
+            self._is_sampling = True
 
     def sample(self):
         """
@@ -92,23 +114,23 @@ class Agent(mesa.Agent):
         x, y = self.pos
 
         if self.model.random.random() < self.model.resource_distribution[x, y]:
-            self.collected_resource += 1
-            self.sampling_sequence.append(1)
+            self._collected_resource += 1
+            self._sampling_sequence.append(1)
         else:
-            self.sampling_sequence.append(0)
+            self._sampling_sequence.append(0)
 
         # finish sampling and update observations
-        if len(self.sampling_sequence) == self.sampling_length:
+        if len(self._sampling_sequence) == self.sampling_length:
             self.update_local_observations()
 
             # finish sampling
-            self.is_sampling = False
+            self._is_sampling = False
 
             # reset sampling sequence
-            self.sampling_sequence = []
+            self._sampling_sequence = []
 
             # update local search counter
-            self.local_search_count += 1
+            self._local_search_count += 1
 
     def update_local_observations(self):
         """
@@ -118,7 +140,7 @@ class Agent(mesa.Agent):
 
         # replace previous observation with the new observation
         # TODO: implement a proper running average
-        self.observations[x, y] = np.mean([np.mean(self.sampling_sequence), self.observations[x, y]])
+        self._observations[x, y] = np.mean([np.mean(self._sampling_sequence), self._observations[x, y]])
 
     def update_meso_social_density(self):
         # get neighboring agents
@@ -126,109 +148,87 @@ class Agent(mesa.Agent):
                                                      radius=self.model.grid.width)
         # get discounted smoothed locations map of other agents
         # TODO: use the observation history with the discount factor to incorporate the temporal aspect
-        self.meso_soc_density.fill(0)
+        self._meso_soc.fill(0)
         for agent in other_agents:
             if agent.is_sampling:
-                x, y = agent.pos
-                self.meso_soc_density[x // self.meso_grid_step, y // self.meso_grid_step] += 1
+                self._meso_soc[*agent.meso_pos] += 1
 
         # normalize to make the sum equal to 1
         # TODO: add information about social density on the previous step
-        self.meso_soc_density /= np.sum(self.meso_soc_density) + 1e-6
+        self._meso_soc = self.normalize(self._meso_soc)
 
     def update_meso_environmental_belief(self):
-        x_slice, y_slice, meso_x, meso_y = self._micro_slice_from_meso()
-        max_obs = np.max(self.observations[x_slice, y_slice])
+        meso_x, meso_y = self.meso_pos
+        max_obs = np.max(self._observations[*self.model.grid.micro_slice_from_meso(meso_x, meso_y)])
 
         # TODO: add information about the previous observation with the discount factor
-        self.meso_env_belief[meso_x, meso_y] = np.mean([max_obs, self.meso_env_belief[meso_x, meso_y]])
+        self._meso_env[meso_x, meso_y] = np.mean([max_obs, self._meso_env[meso_x, meso_y]])
+        return self.normalize(self._meso_env)
 
-    def update_meso_random_preference(self):
+    def generate_random_preference(self):
         # generate random values for the preferences
-        self.meso_rand_array = np.random.random(size=(self.meso_grid_width, self.meso_grid_height))
+        rand_array = np.random.random(size=(self.model.grid.meso_width, self.model.grid.meso_height))
 
         # normalize
-        self.meso_rand_array /= np.sum(self.meso_rand_array) + 1e-6
+        return self.normalize(rand_array)
 
     def update_meso_beliefs(self):
         self.update_meso_social_density()
-        self.update_meso_environmental_belief()
-        self.update_meso_random_preference()
+        env_info = self.update_meso_environmental_belief()
+        random_preference = self.generate_random_preference()
 
         # combine beliefs
-        self.meso_belief = self.alpha_social * self.meso_soc_density + \
-                           self.alpha_env * (self.meso_env_belief / (np.sum(self.meso_env_belief) + 1e-6)) + \
-                           self.alpha_random * self.meso_rand_array
-
-        _, _, meso_x, meso_y = self._micro_slice_from_meso()
+        belief = self.w_soc * self._meso_soc + self.w_env * env_info + self.w_rand * random_preference
 
         # discount the belief with the distance from the current location
-        self.meso_belief = discount_by_distance(self.meso_belief, (meso_x, meso_y), discount_factor=0.5)
+        belief_discounted = discount_by_distance(belief, self.meso_pos, discount_factor=0.5)
 
-        # normalize
-        self.meso_belief /= np.sum(self.meso_belief) + 1e-6
+        self._meso_combined = self.normalize(belief_discounted)
 
     def global_displacement(self):
         self.update_meso_beliefs()
 
         # get the peak of the relocation map and find the closest empty cell to the destination
-        dy, dx = find_peak(self.meso_belief)
-        rand_x = dx * self.meso_grid_step + self.model.random.randint(0, self.meso_grid_step - 1)
-        rand_y = dy * self.meso_grid_step + self.model.random.randint(0, self.meso_grid_step - 1)
-        self.destination = self._closest_empty_cell((rand_x, rand_y))
+        dy, dx = find_peak(self._meso_combined)
+        self._destination = self._get_random_micro_cell_in_meso_grid(dx, dy)
 
         # reset local search counter
-        self.local_search_count = 0
+        self._local_search_count = 0
 
     def local_displacement(self):
         # select random location within the meso-scale grid cell
-        _, _, meso_x, meso_y = self._micro_slice_from_meso()
-        x_rand = self.model.random.randint(meso_x * self.meso_grid_step, (meso_x + 1) * self.meso_grid_step - 1)
-        y_rand = self.model.random.randint(meso_y * self.meso_grid_step, (meso_y + 1) * self.meso_grid_step - 1)
-        self.destination = self._closest_empty_cell((x_rand, y_rand))
+        self._destination = self._get_random_micro_cell_in_meso_grid(*self.meso_pos)
 
     def choose_next_action(self):
         """
         Choose the next action for the agent.
         """
-        if self.is_moving and not self.is_sampling:
+        if self._is_moving and not self._is_sampling:
             self.move()
             return
 
-        if self.is_sampling and not self.is_moving:
+        if self._is_sampling and not self._is_moving:
             self.sample()
 
-        if not self.is_sampling and not self.is_moving:  # this is also the case when the agent is initialized
-            counter_done = self.local_search_count >= self.local_search_counter
-            good_spot_found = self.observations[self.pos] > self.relocation_threshold
+        if not self._is_sampling and not self._is_moving:  # this is also the case when the agent is initialized
+            counter_done = self._local_search_count >= self.local_search_counter
+            good_spot_found = self._observations[self.pos] > self.relocation_threshold
 
             if good_spot_found:
-                self.is_sampling = True
+                self._is_sampling = True
                 self.sample()
             elif not counter_done:
                 self.local_displacement()
-                self.is_moving = True
+                self._is_moving = True
             else:
                 self.global_displacement()
-                self.is_moving = True
-                if self.visualization:
-                    self.debug_plot()
+                self._is_moving = True
 
-        if self.is_moving and self.is_sampling:
+        if self._is_moving and self._is_sampling:
             raise ValueError("Agent is both sampling and moving.")
 
     def step(self):
         self.choose_next_action()
-
-    def _micro_slice_from_meso(self):
-        # find max observation in the micro-scale grid
-        x, y = self.pos
-        meso_x = x // self.meso_grid_step
-        meso_y = y // self.meso_grid_step
-        x_slice = slice(meso_x * self.meso_grid_step, (meso_x + 1) * self.meso_grid_step)
-        y_slice = slice(meso_y * self.meso_grid_step, (meso_y + 1) * self.meso_grid_step)
-
-        return x_slice, y_slice, meso_x, meso_y
 
     def _closest_empty_cell(self, destination: tuple[int, int]):
         radius = 2
@@ -243,6 +243,12 @@ class Agent(mesa.Agent):
     def _get_empty_cells(self, destination, radius=1):
         neighbors = self.model.grid.get_neighborhood(destination, moore=True, include_center=True, radius=radius)
         return [cell for cell in neighbors if self.model.grid.is_cell_empty(cell)]
+
+    def _get_random_micro_cell_in_meso_grid(self, meso_x: int, meso_y: int):
+        slice_x, slice_y = self.model.grid.micro_slice_from_meso(meso_x, meso_y)
+        x = self.model.random.randint(slice_x.start, slice_x.stop - 1)
+        y = self.model.random.randint(slice_y.start, slice_y.stop - 1)
+        return self._closest_empty_cell((x, y))
 
     @staticmethod
     def _information_weights(w_soc: float, w_env: float) -> tuple[float, float, float]:
@@ -262,14 +268,6 @@ class Agent(mesa.Agent):
 
         return w_soc, w_env, w_rand
 
-    def debug_plot(self):
-        if self.unique_id == 1:
-            self.model.agent_raw_observations = self._observations
-
-            kron_shape = (self.meso_grid_step, self.meso_grid_step)
-            d_type = self._meso_belief.dtype
-
-            self.model.relocation_map = np.kron(self._meso_belief, np.ones(kron_shape, dtype=d_type))
-            self.model.agent_raw_soc_observations = np.kron(self._meso_soc, np.ones(kron_shape, dtype=d_type))
-            self.model.agent_raw_env_belief = np.kron(self._meso_env, np.ones(kron_shape, dtype=d_type))
-            self.model.agent_raw_rand_array = np.kron(self._meso_rand_array, np.ones(kron_shape, dtype=d_type))
+    @staticmethod
+    def normalize(arr: np.ndarray) -> np.ndarray:
+        return arr / (np.sum(arr) + 1e-6)
--- ice_fishing_abm_1/ice_fishing_abm_1/space.py
+++ ice_fishing_abm_1/ice_fishing_abm_1/space.py
@@ -9,4 +9,18 @@ class MultiMicroMesoGrid(MultiGrid):
         self.meso_scale_step = meso_scale_step
         assert width % meso_scale_step == 0, "width must be divisible by meso_scale_step"
         assert height % meso_scale_step == 0, "height must be divisible by meso_scale_step"
-        self.meso_grid = MultiGrid(width // meso_scale_step, height // meso_scale_step, torus)
+        self.meso_width = width // meso_scale_step
+        self.meso_height = height // meso_scale_step
+
+    def meso_coordinate(self, x: int, y: int) -> tuple[int, int]:
+        """
+        Return the meso grid coordinates for the given micro grid coordinates.
+        """
+        return x // self.meso_scale_step, y // self.meso_scale_step
+
+    def micro_slice_from_meso_coordinate(self, x: int, y: int) -> tuple[slice, slice]:
+        """
+        Return the micro grid slice for the given meso grid coordinates.
+        """
+        return slice(x * self.meso_scale_step, (x + 1) * self.meso_scale_step), \
+            slice(y * self.meso_scale_step, (y + 1) * self.meso_scale_step)
