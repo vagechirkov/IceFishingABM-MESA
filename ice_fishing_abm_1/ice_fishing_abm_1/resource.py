8ebb4ac644e55fac1a7edee7f12c64ba561d7de6
valery <vagechirkov@gmail.com>
2023-11-03 16:57:51 +0100

    keep abundance constant

--- ice_fishing_abm_1/ice_fishing_abm_1/resource.py
+++ ice_fishing_abm_1/ice_fishing_abm_1/resource.py
@@ -2,32 +2,46 @@ import mesa
 
 
 class Resource(mesa.Agent):
-    def __init__(self, unique_id, model, radius=5, max_value=100, current_value=50):
+    def __init__(self, unique_id, model, radius=5, max_value=100, current_value=50, keep_overall_abundance=True,
+                 neighborhood_radius=20):
         super().__init__(unique_id, model)
         self.radius: int = radius
         self.model = model
         self.max_value: int = max_value
         self.current_value: int = current_value
+        self.keep_overall_abundance: bool = keep_overall_abundance
+        self.neighborhood_radius: int = neighborhood_radius
 
     def catch_probability(self):
+        # this relation is linear for now but might be more realistic if it is sigmoidal
         return self.current_value / self.max_value
 
     def catch(self):
         if self.model.random.random() < self.catch_probability():
             self.current_value -= 1
-            self._add_resource_to_neighbour()
+            if self.keep_overall_abundance:
+                self._add_resource_to_neighbour()
             return True
         else:
             return False
 
     def _add_resource_to_neighbour(self):
+        """Add one resource to the closest neighbor"""
         neighbours = self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False,
-                                                      radius=self.model.grid.width)
+                                                      radius=self.neighborhood_radius)
+
+        resources = []
+
         for neighbour in neighbours:
-            neighbour_agent = self.model.grid.get_cell_list_contents([neighbour])[0]
-            if isinstance(neighbour_agent, Resource):
-                neighbour_agent.current_value += 1
-                break
+            for agent in self.model.grid.get_cell_list_contents([neighbour]):
+                if isinstance(agent, Resource):
+                    resources.append(agent)
+
+        # if the current resource is not the only resource in the neighborhood
+        if len(resources) > 0:
+            # find closest resource
+            closest_resource = min(resources, key=lambda x: self.model.grid.get_distance(self.pos, x.pos))
+            closest_resource.current_value += 1
 
     def step(self):
         pass
