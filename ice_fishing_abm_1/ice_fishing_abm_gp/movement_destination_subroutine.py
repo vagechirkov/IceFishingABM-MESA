81557ac529744302de97796793a2b172290de6a2
valery <vagechirkov@gmail.com>
2024-07-19 15:08:36 +0200

    add two main subroutines

---
+++ ice_fishing_abm_1/ice_fishing_abm_gp/movement_destination_subroutine.py
@@ -0,0 +1,12 @@
+
+
+class MovementDestinationSubroutine:
+    def __init__(self):
+        pass
+
+
+class GPMovementDestinationSubroutine(MovementDestinationSubroutine):
+    def __init__(self):
+        super().__init__()
+
+
---
+++ ice_fishing_abm_1/ice_fishing_abm_gp/tests/test_patch_evaluation_subroutine.py
@@ -0,0 +1,9 @@
+from ice_fishing_abm_1.ice_fishing_abm_gp.patch_evaluation_subroutine import PatchEvaluationSubroutine
+
+
+def test_patch_evaluation_subroutine_default():
+    pes = PatchEvaluationSubroutine()
+
+    assert pes.stay_on_patch(1, 10, 5) is True
+    assert pes.stay_on_patch(5, 10, 5) is True
+    assert pes.stay_on_patch(6, 10, 5) is False
