9f644c4a0bb269a643bb3a20c3aa19392f6ff0ad
valery <vagechirkov@gmail.com>
2024-09-17 09:44:57 +0200

    fix the test for patch_evaluation_subroutine

--- ice_fishing_abm_1/ice_fishing_abm_gp/tests/test_patch_evaluation_subroutine.py
+++ ice_fishing_abm_1/ice_fishing_abm_gp/tests/test_patch_evaluation_subroutine.py
@@ -2,10 +2,10 @@ from ice_fishing_abm_1.ice_fishing_abm_gp.patch_evaluation_subroutine import Pat
 
 
 def test_patch_evaluation_subroutine_default():
-    pes = PatchEvaluationSubroutine()
+    pes = PatchEvaluationSubroutine(threshold=5)
 
-    assert pes.stay_on_patch(1, 10, 5) is True
-    assert pes.stay_on_patch(5, 10, 5) is True
-    assert pes.stay_on_patch(6, 10, 5) is False
+    assert pes.stay_on_patch(1, 10) is True
+    assert pes.stay_on_patch(5, 10) is True
+    assert pes.stay_on_patch(6, 10) is False
 
 
