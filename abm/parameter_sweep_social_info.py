af5c107953eb59699865ebb58d154746086d375a
valery <vagechirkov@gmail.com>
2025-11-21 18:21:27 +0100

    social param sweep: change default tau value

--- abm/parameter_sweep_social_info.py
+++ abm/parameter_sweep_social_info.py
@@ -11,7 +11,8 @@ def run_simulations_social_info_abundance(n_repetitions: int = 1):
         "number_of_agents": 6,
         "simulation_length_minutes": 180,
         "fish_abundance": [2.5, 3.0, 3.5],
-        "spot_selection_w_social": [0.042, 0.2, 0.8, 2.4],  # 1:4, 2:4, 3:4 ratio of soc to private weights
+        "spot_selection_tau": 0.1,
+        "spot_selection_w_social": [0, 0.25, 0.75, 2.25],  # 1:4, 2:4, 3:4 ratio of soc to private weights
         "spot_selection_social_info_quality": ["sampling", "consuming"]
     }
 
